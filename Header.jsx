1c6cb20e679eedeb8af76f3ca15b133362250ac2
Elga2000 <85423555+Elga2000@users.noreply.github.com>
2021-06-06 11:59:32 +0530

    Add files via upload
    # SHAPEAI Javascript and Reactjs BOOTCAMP
    
    Hi I made this project during the 7 Days Free Bootcamp, conducted by <b> SHAPEAI
    
    </b>.
    
    The instructor during the session was Mr. Shaurya Sinha (Data Analyst Intern at Jio). I got to
    
    learn a lot during these 7 days and it was an amazing experience learning with SHAPEAI.
    
    <br><br>Here's the link for you to watch the sessions as well<br>
    
    <a href="https://www.youtube.com/playlist?list=PL7zl8TDRnbulLetcbkthT0p_IzwgRAYbu"> <img src="https://github.com/ShapeAI/PYTHON-AND-DATA-ANALYTICS/blob/main/YOUTUBE%20THUMBNAIL-4.png"> </a>
    
    <br>I got to have hands on experience on:
    
    <li>JavaScript
    
    <li>HTML
    
    <li>React.js
    
    <br>during these 7 days, and everything was explained from the very basics so that
    
    anyone with zero experience on programming can learn.
    
    I enjoyed these 7 days, you can as well. To register for next free 7 days bootcamp, visit:
    
    <a href="https://www.shapeai.tech"> www.shapeai.tech</a>
    
    or follow SHAPEAI on:
    
    <li><a href=
    
    "https://in.linkedin.com/company/shapeai">LinkedIn</a>
    
    <li><a href=
    
    "https://www.instagram.com/shape.ai/?hl=en">Instagram</a>
    
    <li><a
    
    href=
    
    "https://www.youtube.com/channel/UCTUvDLTW9meuDXWcbmISPdA">YouTu
    
    be</a>

---
+++ Header.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+function Header()
+{
+  return(<div>
+    <header/>
+      <h1> ShapeAI </h1>   
+  </div>);
+  }
+export default Header;
---
+++ Note.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+function Note()
+{
+  return(<div className="note">
+    <h1>Basic Web Development </h1>
+    <p> This was a 7 days bootcamp for basic web development 
+      using Javascript and React js,taught by Mr. Shaurya Sinha.
+      </p>
+      </div>);
+}
+export default Note;
